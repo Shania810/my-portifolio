1a2eaed4acb9a534b8e708c202300d43b84cfaec
Shania <shaniaantezana@gmail.com>
2022-12-27 12:12:52 -0400

    create design and file Design-antd.js,change Home.js

---
+++ src/design/Design-antd.js
@@ -0,0 +1,3 @@
+import { Typography,Button,Space} from "antd";
+const {Title} = Typography
+export {Title,Button,Space}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
 import React from 'react'
-
+import {Title,Button, Space} from '../design/Design-antd'
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <Title>Olá,Meu nome é Shania</Title>
+        <Space wrap>
+           <Button type='primary' href='/me' size='large'>Sobre mim</Button>
+           <Button type='primary' href='/projects' size='large'>Meus projetos</Button>
+        </Space>
+    </div>
   )
 }
