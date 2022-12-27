7f4006fb4d23367beb87d80a67156b9e0b749006
Shania <shaniaantezana@gmail.com>
2022-12-27 12:31:04 -0400

    insert component NavBar in app.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import {Routes,Route} from 'react-router-dom';
 import { Home } from './pages/Home';
 import {AboutMe} from './pages/AboutMe';
 import {Projects} from './pages/Projects';
+import { NavBar } from './components/NavBar';
 function App() {
   return (
     <div className="App">
+    <NavBar/>
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/me' element={<AboutMe/>} />
