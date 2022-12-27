542d67a4c1ff487a92cccc924d140a530e4e23a5
Shania <shaniaantezana@gmail.com>
2022-12-27 17:41:31 -0400

    insert Link React-router-dom in NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import {Button} from '../design/Design-antd'
 import { Nav,NavRight } from '../design/Styled-components'
 export const NavBar = () => {
   return (
     <Nav>
-        <Button type='primary' href='/'>Home</Button>
+        <Button type='primary'><Link to='/'>Home</Link></Button>
         <NavRight>
-        <Button type='primary' href='/projects' >Meus projetos</Button>
-        <Button type='primary' href='/me' >Sobre mim</Button>
+        <Button type='primary'><Link to='/projects'>Meus projetos</Link></Button>
+        <Button type='primary'><Link to='/me'>Sobre mim</Link></Button>
         </NavRight>
     </Nav>
   )
