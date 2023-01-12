3cd0f8abc5ed48621d81cda2991e15f55e1ff235
Shania <shaniaantezana@gmail.com>
2023-01-11 22:12:17 -0400

    change AboutMe.js

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Title } from '../design/Design-antd'
+import { Container } from '../design/Styled-components'
 
 export const AboutMe = () => {
   return (
-    <div>AboutMe</div>
+    <Container>
+      <Title>Shania Antezana Sarate</Title>
+      <p>
+        Sou desenvolvedora full-stack que começou na área de desenvolvimento web aos <b>16 anos</b> no momento da minha vida que tinha<br></br>
+        percebido que a <b>carreira militar</b> não era o que eu queria seguir.Minha irmã <b>Melory Ayala</b> me ajudou e mostrou  a área de tecnologia<br></br>
+        (especificamente a <b>programação</b>) por meio de um curso da Udemy que comecei a fazer aulas.Adorei porque eu vi os resultados<br></br>
+        do que fazia em cada comando feito.Isso me fez ter mais curiosidade na área até que descubri <b>Ironhack</b> por meio da minha irmã e decidi tentar fazer <br></br>
+        o <b>bootcamp de web development</b>.Durante o bootcamp,amei mais ainda de forma que toda vez que tinha tarefa do curso,ficava muito feliz<br></br> 
+        quando pegava dados corretos do <b>back-end</b>e adorava quando no <b>front-end</b> eu fazia requisitos do back-end,recebia dados no console.Atualmente,estou fazendo<br></br> 
+        projeto para <b>Atacado Safira Susan</b> empresa dos meus pais para facilitar a vida deles no controle da empresa(lucro,comissão de vendedores,estoque,pedidos).
+      </p>
+    </Container>
   )
 }
