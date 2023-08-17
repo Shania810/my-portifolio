a7191f5d56a4ee266cdf9150e789c9afe201661c
Shania <shaniaantezana@gmail.com>
2023-08-17 15:54:14 -0300

    Created folder design and file component

---
+++ src/design/Component/Component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Component = () => {
+  return (
+    <div>
+        <div id="blue" ></div>
+        <div id="purple" ></div>
+        <div id="green" ></div>
+    </div>
+  )
+}
