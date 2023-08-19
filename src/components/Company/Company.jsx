7001036d7c8ac4438618567cad933f5aa113da5b
Shania <shaniaantezana@gmail.com>
2023-08-19 10:32:46 -0300

    Inserted design in component company and changed style of the section Experiences, fix bugs

--- src/components/Company/Company.jsx
+++ src/components/Company/Company.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './style.css'
 
 export const Company = ({ company }) => {
     const { image, title } = company
     return (
-        <figure>
+        <div class='figure-company' >
             <img src={image} alt={image} />
-            <figcaption>{title}</figcaption>
-        </figure>
+            <p>{title}</p>
+        </div>
     )
 }
