8b4e436b521f7e6cf3b72d889fdd3a7425ee90bf
Shania <shaniaantezana@gmail.com>
2023-08-19 08:46:40 -0300

    Inserted design in component Experience

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Company } from '../Company'
 import { Badge } from '../Badge'
+import './style.css'
 
 export const Experience = ({ experience }) => {
     const { company, description, duration, skills } = experience
     return (
-        <div>
+        <div id='experience' >
             <Company company={company} />
             <time>{duration}</time>
             <p>{description}</p>
