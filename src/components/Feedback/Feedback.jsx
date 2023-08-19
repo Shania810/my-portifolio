477f26ba897e296358e09578f673a086d81a3392
Shania <shaniaantezana@gmail.com>
2023-08-19 11:59:32 -0300

    Inserted design in component Feedback

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Person } from '../Person'
+import './style.css'
 
 export const Feedback = ({ feedback }) => {
     const { person, description } = feedback
     return (
-        <>
+        <div className='feedback' >
             <Person person={person} />
             <blockquote>{description}</blockquote>
-        </>
+        </div>
     )
 }
