ee11c56b047746cc27687fe4bf28267981a7d314
Shania <shaniaantezana@gmail.com>
2023-08-19 14:44:34 -0300

    Inserted  design in component Person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import './style.css'
+import { CgQuote } from 'react-icons/cg'
 
 export const Person = ({ person }) => {
     const { name, image } = person
     return (
-        <figure>
-            <img src={image} alt={image} />
-            <figcaption>{name}</figcaption>
+        <figure className='figure-person' >
+            <div>
+                <img src={image} alt={image} />
+                <figcaption>{name}</figcaption>
+            </div>
+            <div>
+                <CgQuote size='110' color='#DEDEDE' />
+            </div>
         </figure>
     )
 }
