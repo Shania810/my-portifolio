e95ea23ad0a35c3f472b2dcfbf07503ad47be7a3
Shania <shaniaantezana@gmail.com>
2023-08-18 22:05:52 -0300

    Created folder Experience and file Experience.jsx

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Company } from './Company'
-import { Badge } from './Badge'
+import { Company } from '../Company'
+import { Badge } from '../Badge'
 
 export const Experience = ({ experience }) => {
     const { company, description, duration, skills } = experience
--- src/sections/Experiences/Experiences.jsx
+++ src/sections/Experiences/Experiences.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import api from '../../utils/api.utils'
-import { Experience } from '../../components/Experience'
+import { Experience } from '../../components/Experience/Experience'
 import { BsAsterisk } from 'react-icons/bs'
 import './style.css'
 
