1cc03e8306a5b753788695a5ae61acf6651b6042
Shania <shaniaantezana@gmail.com>
2023-08-21 08:40:31 -0300

    Creted folder RowSkill and component RowSkill, Inserted component in section Main

---
+++ src/components/RowSkill/RowSkill.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import api from '../../utils/api.utils'
+import { BiSolidCircle } from 'react-icons/bi'
+import './style.css'
+
+export const RowSkill = () => {
+    const [skills, setSkills] = useState([])
+
+    useEffect(() => {
+        const allSkills = async () => {
+            try {
+                const skills = await api.getSkills()
+                setSkills(skills.slice(0, 10))
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        allSkills()
+    }, [])
+
+    return (
+        <div id='rowSkill' >
+            {skills.map((skill, index) => {
+                return <div>
+                    <span>{skill.title}</span>
+                    {index !== skills.length - 1 && <BiSolidCircle color='#CECECE' size='10' />}
+                </div>
+            })}
+        </div>
+    )
+}
--- src/sections/Main/Main.jsx
+++ src/sections/Main/Main.jsx
@@ -2,19 +2,23 @@ import React from 'react'
 import imagePerson from '../../images/imagePerson.png'
 import { Component } from '../../design/Component/Component'
 import './style.css'
+import { RowSkill } from '../../components/RowSkill/RowSkill'
 
 export const Main = () => {
     return (
         <section>
             <main>
-                <div id='title' >
-                    <h1>A junior</h1>
-                    <h1>Full-stack developer</h1>
-                    <Component />
+                <div id='title-image' >
+                    <div id='title' >
+                        <h1>A junior</h1>
+                        <h1>Full-stack developer</h1>
+                        <Component />
+                    </div>
+                    <div id='image' >
+                        <img src={imagePerson} alt={imagePerson} />
+                    </div>
                 </div>
-                <div id='image' >
-                    <img src={imagePerson} alt={imagePerson} />
-                </div>
+                <RowSkill />
             </main>
         </section>
     )
