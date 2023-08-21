1c6575616e90c73ac29c4b874baa493f2dc4f8e7
Shania <shaniaantezana@gmail.com>
2023-08-21 13:03:14 -0300

    added cjanges component project and your style.css

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './style.css'
 
 export const Project = ({ project }) => {
-    const { title, image, description } = project
+    const { title, image, description, link } = project
     return (
-        <div class='project' >
-            <img src={image} alt={image} />
-            <h2>{title}</h2>
-            <p>{description}</p>
+        <div className='project' >
+            <a href={link} target='_blank' rel='noreferrer' >
+                <img src={image} alt={image} />
+                <h2>{title}</h2>
+                <p>{description}</p>
+            </a>
         </div>
     )
 }
