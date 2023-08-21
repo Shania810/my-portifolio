871b49dd139c8d4c752bf0d33c2e41912d334638
Shania <shaniaantezana@gmail.com>
2023-08-21 14:38:26 -0300

    added changes in section Navbar and your style.css, inserted new image in folder images

--- src/sections/Navbar/Navbar.jsx
+++ src/sections/Navbar/Navbar.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import logo from '../../images/logo.png'
 import { Link } from '../../components/Link/Link'
 import './style.css'
+import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
     return (
         <nav>
             <div>
-                <img src={logo} alt={logo} />
+                <NavLink to='/' >
+                    <img src={logo} alt={logo} />
+                </NavLink>
                 <div id='links'>
                     <Link url="https://github.com/Shania810" value="Github" />
                     <Link url="https://www.linkedin.com/in/shania-antezana/" value="Linkedin" />
